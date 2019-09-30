12e087a75c5077dfba464c3066607f89eac1165f
Alex Pinheiro <alex.pinheiro@compasso.com.br>
2019-09-30 16:52:18 -0300

    Conexão com MySQL

--- api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-
-const cors = require('cors');
-const api = express();
-
-const port = 3333;
-
---
+++ rest-api/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const cors = require('cors');
+const api = express();
+const port = 3000;
+const router = express.Router();
+
+//Trata as requisições da API
+api.use(cors());
+
+api.use(bodyparser.urlencoded({ extended: true }));
+api.use(bodyparser.json({ limit: '20mb',extended: true }));
+
+router.get("/",(req, resp) => resp.json({
+    mensagem: '=> API Online...'
+}));
+
+api.use("/", router);
+api.listen(port);
+console.log("Run API Express");
